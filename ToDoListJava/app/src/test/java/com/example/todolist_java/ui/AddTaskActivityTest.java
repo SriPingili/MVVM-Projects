7465157ee694f71ce57fe745996ab5c8e8fe10a9
Srijith <srijithreddy.pingili@cerner.com>
2020-06-06 07:21:45 -0500

    ToDO List -  Added test cases

--- ToDoListJava/app/src/test/java/com/example/todolist_java/ui/AddTaskActivityTest.java
+++ ToDoListJava/app/src/test/java/com/example/todolist_java/ui/AddTaskActivityTest.java
@@ -1,10 +1,8 @@
 package com.example.todolist_java.ui;
 
 import android.content.Intent;
-import android.database.Observable;
 
 import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
@@ -15,7 +13,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Captor;
 import org.mockito.Mock;
@@ -25,12 +22,9 @@ import org.robolectric.RobolectricTestRunner;
 
 import java.util.Date;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 public class AddTaskActivityTest {
@@ -60,19 +54,30 @@ public class AddTaskActivityTest {
 
     }
 
+    /*https://medium.com/exploring-android/android-architecture-components-testing-your-viewmodel-livedata-70177af89c6e*/
+    @Test
+    public void testOnChangeLiveData() {
+        TaskEntry taskEntry = new TaskEntry(123, "description", 1, new Date());
+        MutableLiveData<TaskEntry> liveDataTest = new MutableLiveData<>();
+        liveDataTest.setValue(taskEntry);
+        spyActivity.setTestViewModel(addTaskViewModel);
+        liveDataTest.observeForever(observable);
+
+        doReturn(liveDataTest).when(addTaskViewModel).getTaskEntry();
+
+
+        spyActivity.testLiveData();
+
+
+        verify(observable).onChanged(taskEntry);
+    }
 
     /*
     https://medium.com/tribalscale/testing-viewmodels-and-activities-in-your-mvvm-app-5bf946559258
     * */
     @Test
-    public void testLiveData() {
+    public void testLiveDataPopulateUICall() {
         TaskEntry taskEntry = new TaskEntry(123, "description", 1, new Date());
-
-//        MutableLiveData<TaskEntry> liveData = new MutableLiveData<>();
-
-
-//        liveData.observeForever(observable);
-
         spyActivity.setTestViewModel(addTaskViewModel);
 
         doReturn(liveData).when(addTaskViewModel).getTaskEntry();
@@ -80,22 +85,12 @@ public class AddTaskActivityTest {
 
         spyActivity.testLiveData();
 
-
-
-
         verify(liveData).observe(ArgumentMatchers.any(LifecycleOwner.class), teamsObserverCaptor.capture());
 
 
-
-//                liveData.setValue(taskEntry);
         teamsObserverCaptor.getValue().onChanged(taskEntry);
         verify(liveData).removeObserver(teamsObserverCaptor.getValue());
         verify(spyActivity).populateUI(taskEntry);
-
-
-//        verify(observable).onChanged(taskEntry);
-//        verify(spyActivity).populateUI(taskEntry);
-
 
     }
 }
